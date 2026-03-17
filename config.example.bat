8d49db27fa06430a4c4c4fac1087ca495208772c
seul <19603889396@163.com>
2026-03-18 00:52:36 +0800

    修改配置bat，增加冗余性

--- config.example.bat
+++ config.example.bat
@@ -5,6 +5,7 @@ REM ========================================
 
 REM ==== YOUR TOMCAT PATH ====
 set CATALINA_HOME=YOUR_TOMCAT_PATH_HERE
+set TOMCAT_HOME=%CATALINA_HOME%
 
 REM ==== APP NAME (optional) ====
 set APP_NAME=groupproject
